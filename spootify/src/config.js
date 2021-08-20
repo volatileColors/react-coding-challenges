301dbac98c866912953f145789a1df203f287d38
volatileColors <volatilecolors@gmail.com>
2021-08-20 13:16:54 +0545

    Ignore file cleanup

--- spootify/src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '590aaadbe07c4d7fae6f4c76ad6bb488',
-    clientSecret: '024d870be4994bcb9a9cca5d5122abea'
-  }
-}
