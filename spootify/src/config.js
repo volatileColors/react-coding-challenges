ab4f921d929b1809b3e0ca11864feebc46fbeb8d
volatileColors <volatilecolors@gmail.com>
2021-08-20 12:59:28 +0545

    Spootify challenge solved

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: '590aaadbe07c4d7fae6f4c76ad6bb488',
+    clientSecret: '024d870be4994bcb9a9cca5d5122abea'
   }
 }
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+import axios from 'axios';
+import api from 'config';
 
 export default class Discover extends Component {
   constructor() {
@@ -9,8 +11,92 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
+      access_token: ''
     };
+  }
+
+  componentDidMount() {
+    this.authorizeAPI().then((token) => {
+      this.setState({ access_token: token})
+
+      this.performAllCalls();
+    });
+  }
+
+  async authorizeAPI() {
+    const authUrl = api.api['authUrl'];
+    const clientId = api.api['clientId'];
+    const clientSecret = api.api['clientSecret'];
+
+    const res = await axios({
+      url: authUrl,
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+      },
+      data: new URLSearchParams({
+        grant_type: 'client_credentials'
+      })
+    });
+    return res.data['access_token'];
+  }
+
+  async performAllCalls() {
+    let [releases, playlists, categories] = await Promise.all([this.getNewReleases(), this.getPlaylists(), this.getCategories()]);
+
+    this.setState({ newReleases: releases.albums['items'] });
+    this.setState({ playlists: playlists.playlists['items']});
+    this.setState({ categories: categories.categories['items']});
+  }
+
+  getNewReleases() {
+    return new Promise((resolve, reject) => {
+      return axios({
+        url: api.api['baseUrl'] + '/browse/new-releases',
+        method: 'get',
+        headers: {
+          'Authorization': `Bearer ${this.state.access_token}`
+        },
+      }).then((res) => {
+        resolve(res.data);
+      }).catch((error) => {
+        resolve(null);
+      })
+    })
+  }
+
+  getPlaylists() {
+    return new Promise((resolve, reject) => {
+      return axios({
+        url: api.api['baseUrl'] + '/browse/featured-playlists',
+        method: 'get',
+        headers: {
+          'Authorization': `Bearer ${this.state.access_token}`
+        },
+      }).then((res) => {
+        resolve(res.data);
+      }).catch((error) => {
+        resolve(null);
+      })
+    })
+  }
+
+  getCategories() {
+    return new Promise((resolve, reject) => {
+      return axios({
+        url: api.api['baseUrl'] + '/browse/categories',
+        method: 'get',
+        headers: {
+          'Authorization': `Bearer ${this.state.access_token}`
+        },
+      }).then((res) => {
+        resolve(res.data);
+      }).catch((error) => {
+        resolve(null);
+      })
+    })
   }
 
   render() {
